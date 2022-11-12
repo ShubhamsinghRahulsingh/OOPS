001e013b9851069ef5d81b9b3c9ff4a3e16a8fa9
ShubhamsinghRahulsingh <shubh234singh@gmail.com>
2022-11-12 12:40:40 +0530

    (Shubham)Add: Project folder structure created

---
+++ OOPSPrograms/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace OOPSPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the OOPS Programs");
+        }
+    }
+}
