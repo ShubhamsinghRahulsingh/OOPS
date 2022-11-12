e2989fa0c26fdfb3f049e1551cdbd430979b74c5
ShubhamsinghRahulsingh <shubh234singh@gmail.com>
2022-11-12 16:42:54 +0530

    (Shubham)Add: Json inventory data management of rice,wheat and pulses

--- OOPSPrograms/Program.cs
+++ OOPSPrograms/Program.cs
@@ -1,11 +1,15 @@
-﻿using System;
+﻿using OOPSPrograms.InventoryDataManagement;
+using System;
 namespace OOPSPrograms
 {
     class Program
     {
+        static string jsonFilePath = @"D:\GitRepository\OOPS\OOPSPrograms\InventoryDataManagement\Inventory.json";
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the OOPS Programs");
+            InventoryDetailManagement inventory= new InventoryDetailManagement();
+            inventory.ReadJSONFile(jsonFilePath);
         }
     }
 }
